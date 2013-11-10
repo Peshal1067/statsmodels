88ce49c22a917d6f67dbc9b3f9e3d8ff718cbc98
Paul Hobson <pmhobson@gmail.com>
2013-11-10 10:29:32 -0800

    TST: decorating methods, not test classes + fixed a function call

--- statsmodels/graphics/tests/test_gofplots.py
+++ statsmodels/graphics/tests/test_gofplots.py
@@ -19,57 +19,73 @@ except:
 
 class _base_probplot:
     def base_setup(self):
-        self.fig, self.ax = plt.subplots()
+        if have_matplotlib:
+            self.fig, self.ax = plt.subplots()
         self.other_array = np.random.normal(size=self.prbplt.data.shape)
         self.other_prbplot = sm.ProbPlot(self.other_array)
 
     def teardown():
-        plt.close('all')
+        if have_matplotlib:
+            plt.close('all')
 
+
+    @dec.skipif(not have_matplotlib)
     def test_qqplot(self):
         self.fig = self.prbplt.qqplot(ax=self.ax, line=self.line)
 
+    @dec.skipif(not have_matplotlib)
     def test_ppplot(self):
         self.fig = self.prbplt.ppplot(ax=self.ax, line=self.line)
 
+    @dec.skipif(not have_matplotlib)
     def test_probplot(self):
         self.fig = self.prbplt.probplot(ax=self.ax, line=self.line)
 
+    @dec.skipif(not have_matplotlib)
     def test_qqplot_other_array(self):
         self.fig = self.prbplt.qqplot(ax=self.ax, line=self.line,
                                         other=self.other_array)
+    @dec.skipif(not have_matplotlib)
     def test_ppplot_other_array(self):
         self.fig = self.prbplt.ppplot(ax=self.ax, line=self.line,
                                         other=self.other_array)
+    @dec.skipif(not have_matplotlib)
     def test_probplot_other_array(self):
         self.fig = self.prbplt.probplot(ax=self.ax, line=self.line,
                                         other=self.other_array)
 
+    @dec.skipif(not have_matplotlib)
     def test_qqplot_other_prbplt(self):
         self.fig = self.prbplt.qqplot(ax=self.ax, line=self.line,
                                         other=self.other_prbplot)
+    @dec.skipif(not have_matplotlib)
     def test_ppplot_other_prbplt(self):
         self.fig = self.prbplt.ppplot(ax=self.ax, line=self.line,
                                         other=self.other_prbplot)
+    @dec.skipif(not have_matplotlib)
     def test_probplot_other_prbplt(self):
         self.fig = self.prbplt.probplot(ax=self.ax, line=self.line,
                                         other=self.other_prbplot)
 
+    @dec.skipif(not have_matplotlib)
     def test_qqplot_custom_labels(self):
         self.fig = self.prbplt.qqplot(ax=self.ax, line=self.line,
                                       xlabel='Custom X-Label',
                                       ylabel='Custom Y-Label')
 
+    @dec.skipif(not have_matplotlib)
     def test_ppplot_custom_labels(self):
         self.fig = self.prbplt.ppplot(ax=self.ax, line=self.line,
                                       xlabel='Custom X-Label',
                                       ylabel='Custom Y-Label')
 
+    @dec.skipif(not have_matplotlib)
     def test_probplot_custom_labels(self):
         self.fig = self.prbplt.probplot(ax=self.ax, line=self.line,
                                         xlabel='Custom X-Label',
                                         ylabel='Custom Y-Label')
 
+    @dec.skipif(not have_matplotlib)
     def test_qqplot_pltkwargs(self):
         self.fig = self.prbplt.qqplot(ax=self.ax, line=self.line,
                                       marker='d', 
@@ -77,6 +93,7 @@ class _base_probplot:
                                       markeredgecolor='white',
                                       alpha=0.5)
 
+    @dec.skipif(not have_matplotlib)
     def test_ppplot_pltkwargs(self):
         self.fig = self.prbplt.ppplot(ax=self.ax, line=self.line,
                                       marker='d', 
@@ -84,6 +101,7 @@ class _base_probplot:
                                       markeredgecolor='white',
                                       alpha=0.5)
 
+    @dec.skipif(not have_matplotlib)
     def test_probplot_pltkwargs(self):
         self.fig = self.prbplt.probplot(ax=self.ax, line=self.line,
                                         marker='d', 
@@ -92,7 +110,6 @@ class _base_probplot:
                                         alpha=0.5)
 
 
-@dec.skipif(not have_matplotlib)
 class test_ProbPlot_Longely(_base_probplot):
     def setup(self):
         self.data = sm.datasets.longley.load()
@@ -103,7 +120,6 @@ class test_ProbPlot_Longely(_base_probplot):
         self.base_setup()
 
 
-@dec.skipif(not have_matplotlib)
 class test_ProbPlot_RandomNormal_Minimal(_base_probplot):
     def setup(self):
         self.data = np.random.normal(loc=8.25, scale=3.25, size=37)
@@ -112,7 +128,6 @@ class test_ProbPlot_RandomNormal_Minimal(_base_probplot):
         self.base_setup()
 
 
-@dec.skipif(not have_matplotlib)
 class test_ProbPlot_RandomNormal_WithFit(_base_probplot):
     def setup(self):
         self.data = np.random.normal(loc=8.25, scale=3.25, size=37)
@@ -121,7 +136,6 @@ class test_ProbPlot_RandomNormal_WithFit(_base_probplot):
         self.base_setup()
 
 
-@dec.skipif(not have_matplotlib)
 class test_ProbPlot_RandomNormal_LocScale(_base_probplot):
     def setup(self):
         self.data = np.random.normal(loc=8.25, scale=3.25, size=37)
@@ -141,7 +155,8 @@ class test_top_level:
         self.other_prbplot = sm.ProbPlot(self.other_array)
 
     def teardown(self):
-        plt.close('all')
+        if have_matplotlib:
+            plt.close('all')
 
     @dec.skipif(not have_matplotlib)
     def test_qqplot():
@@ -154,12 +169,11 @@ class test_top_level:
             # test with `ProbPlot` instances
             fig = sm.qqplot_2samples(self.prbplt, self.other_prbplot, 
                                      line=line)
-
     @dec.skipif(not have_matplotlib)
     def test_qqplot_2samples_arrays():
         # also tests all values for line
         for line in ['r', 'q', '45', 's']:
             # test with arrays
-            fig = sm.qqplot_2samples(x=self.res, self.other_array, line=line)
+            fig = sm.qqplot_2samples(self.res, self.other_array, line=line)
 
 
